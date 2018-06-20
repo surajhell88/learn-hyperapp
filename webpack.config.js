481ed39f574cf5ea382c255da582cb1c19f9c24c
surajhell88 <surajhell88@gmail.com>
2018-06-21 01:33:47 +0530

    First Commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require("webpack");
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname),
+    hot: true,
+    port: 9000
+  }
+};
