3e20675e5f4fe0501475da12300b0a142ce10fbb
surajhell88 <surajhell88@gmail.com>
2018-06-23 09:49:08 +0530

    Adding conditional ternary operator for pushing new items to cart. Also added overlay for showing lint errors on web screen during development.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,22 +1,16 @@
 export default {
-  addToCart: id => ({ cart, productList }) => {
-    const cartItems = [...cart];
-    if (cartItems.indexOf(id) === -1) {
-      cartItems.push(id);
-    }
-    return {
-      cart: cartItems,
-      productList: productList.map((item) => {
-        if (item.id === id) {
-          return {
-            ...item,
-            inCartCount: item.inCartCount + 1,
-          };
-        }
-        return item;
-      }),
-    };
-  },
+  addToCart: id => ({ cart, productList }) => ({
+    cart: cart.indexOf(id) === -1 ? [...cart, id] : [...cart],
+    productList: productList.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          inCartCount: item.inCartCount + 1,
+        };
+      }
+      return item;
+    }),
+  }),
   removeFromCart: id => ({ cart, productList }) => ({
     cart: cart.filter(itemID => itemID !== id),
     productList: productList.map((item) => {
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
     contentBase: path.resolve(__dirname),
     hot: true,
     port: 9000,
-    historyApiFallback: true
+    historyApiFallback: true,
+    overlay: true
   }
 };
